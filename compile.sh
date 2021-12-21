28d92847634e8cff6e7abdeaea5506bb71413fb3
Marian Domanski <mmdski@gmail.com>
2021-12-21 13:53:41 -0600

    update project files

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 emcc test.c -o test.js -O0 \
     -s EXPORTED_FUNCTIONS='["_calloc", "_malloc", "_free"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
-    -s TOTAL_MEMORY=33554432
+    -s INITIAL_MEMORY=33554432
--- test.c
+++ test.c
@@ -1,19 +1,7 @@
 #include <emscripten.h>
 
 EMSCRIPTEN_KEEPALIVE
-float get_ptr_value(float *ptr)
-{
-    return *ptr;
-}
-
-EMSCRIPTEN_KEEPALIVE
-float *get_ptr(float *ptr)
-{
-    return ptr;
-}
-
-EMSCRIPTEN_KEEPALIVE
-void copy_array(int size, float *from, float *to)
+void copy_float(int size, const float *from, float *to)
 {
     for (int i = 0; i < size; i++)
     {
